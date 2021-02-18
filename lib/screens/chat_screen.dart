ea7cbcf30be7e22d39f522b38446f86add8dae53
Lloyd Nicholson <nichlloyd@gmail.com>
2021-02-18 17:28:52 +0200

    Remove import

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_twizza_connect_test/widgets/chat/messages.dart';
 import 'package:flutter_twizza_connect_test/widgets/chat/new_message.dart';
 
